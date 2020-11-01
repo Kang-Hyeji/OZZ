c2b1bfce2a7ec78dd653d0fe88a4b6075dfbbc0b
kkojae <jaejeung3210@gmail.com>
2020-11-01 14:10:16 +0900

    ver2 반응형 마무리 js css html code 변경

--- kkojae/chatbot.js
+++ kkojae/chatbot.js
@@ -1,12 +1,20 @@
 "use strict";
+
 // 누구누구님 Ozz Chat-Bot에 오신걸 환영합니다.
 const user_name = prompt("닉네임을 입력해주세요.");
 const greetingFunc = () => {
-    // let greetingName = document.getElementById("header_main_greeting_name");
     let greetingP = document.getElementById("header_main_greeting_p");
     greetingP.innerHTML = `${user_name}님`+ `<br/>` +`Ozz Chat-Bot에 오신걸 환영합니다.`;
-    // greetingName.append("ods");
-    // greetingP.append("Ozz Chat-Bot에 오신걸 환영합니다.");
+
+    setInterval(() => {
+        const index = document.getElementById("index");
+        index.style.display = "none";
+    }, 2000);
+    
+    setInterval(() => {
+        const body_container = document.getElementById("body_container");
+        body_container.style.display = "block";
+    },2060);    
 }
 greetingFunc();
 
@@ -39,62 +47,64 @@ const imgClickList = document.getElementsByClassName("container_header_img");
 
 const createMinElem = () => {
     let today = new Date();
-    // console.log(`${today.getMonth()}.${today.getDate()} ${today.getHours()}:${today.getMinutes()}`);
     let newMinDiv = document.createElement("div");
     let newMinDivContainer = document.createElement("div");
-    let newMinDivContainerNameTime = document.createElement("div");
+    let newMinDivContainerTextTime = document.createElement("div");
     let newMinImg = document.createElement("img");
     let newMinP = document.createElement("p");
     let newMinNameP = document.createElement("p");
     let newMinTimeP = document.createElement("p");
     newMinDiv.setAttribute("class", "container_content");
     newMinDivContainer.setAttribute("class", "container_content_p");
-    newMinDivContainerNameTime.setAttribute("class", "container_content_p_name_time");
+    newMinDivContainerTextTime.setAttribute("class", "container_content_p_text_time");
     newMinImg.setAttribute("src", "./assets/minjong.png");
     newMinImg.setAttribute("alt", "min_image");
     newMinNameP.setAttribute("class", "container_content_name");
     newMinTimeP.setAttribute("class", "container_content_time");
     newMinP.setAttribute("class", "container_content_text_p");
-    newMinNameP.append("Min~");
+    newMinNameP.append("Ozz min~");
     newMinTimeP.append(`${today.getHours()}:${today.getMinutes()}`);
     newMinP.append("무엇이든 물어보세요!");
     parent_container.appendChild(newMinDiv);
     newMinDiv.appendChild(newMinImg);
     newMinDiv.appendChild(newMinDivContainer);
-    newMinDivContainer.appendChild(newMinDivContainerNameTime);
-    newMinDivContainerNameTime.appendChild(newMinNameP);
-    newMinDivContainerNameTime.appendChild(newMinTimeP);
-    newMinDivContainer.appendChild(newMinP);
+    
+    newMinDivContainer.appendChild(newMinNameP);
+    newMinDivContainer.appendChild(newMinDivContainerTextTime);
+
+    newMinDivContainerTextTime.appendChild(newMinP);
+    newMinDivContainerTextTime.appendChild(newMinTimeP);
 }
 
 const createByungElem = () => {
     let today = new Date();
-    // console.log(`${today.getMonth()}.${today.getDate()} ${today.getHours()}:${today.getMinutes()}`);
     let newByungDiv = document.createElement("div");
     let newByungDivContainer = document.createElement("div");
-    let newByungDivContainerNameTime = document.createElement("div");
+    let newByungDivContainerTextTime = document.createElement("div");
     let newByungImg = document.createElement("img");
     let newByungP = document.createElement("p");
     let newByungNameP = document.createElement("p");
     let newByungTimeP = document.createElement("p");
     newByungDiv.setAttribute("class", "container_content");
     newByungDivContainer.setAttribute("class", "container_content_p");
-    newByungDivContainerNameTime.setAttribute("class", "container_content_p_name_time");
-    newByungImg.setAttribute("src", "./assets/byung.png");
+    newByungDivContainerTextTime.setAttribute("class", "container_content_p_text_time");
+    newByungImg.setAttribute("src", "./assets/gong.jpg");
     newByungImg.setAttribute("alt", "byung_image");
     newByungNameP.setAttribute("class", "container_content_name");
     newByungTimeP.setAttribute("class", "container_content_time");
     newByungP.setAttribute("class", "container_content_text_p");
-    newByungNameP.append("Byung~");
+    newByungNameP.append("Ozz gong~");
     newByungTimeP.append(`${today.getHours()}:${today.getMinutes()}`);
     newByungP.append("무엇이든 물어보세요!");
     parent_container.appendChild(newByungDiv);
     newByungDiv.appendChild(newByungImg);
     newByungDiv.appendChild(newByungDivContainer);
-    newByungDivContainer.appendChild(newByungDivContainerNameTime);
-    newByungDivContainerNameTime.appendChild(newByungNameP);
-    newByungDivContainerNameTime.appendChild(newByungTimeP);
-    newByungDivContainer.appendChild(newByungP);
+
+    newByungDivContainer.appendChild(newByungNameP);
+    newByungDivContainer.appendChild(newByungDivContainerTextTime);
+
+    newByungDivContainerTextTime.appendChild(newByungP);
+    newByungDivContainerTextTime.appendChild(newByungTimeP);
 }
 
 // 민이나 병을 선택했을때! 해당 이미지 크기와 색상변경 추가
@@ -130,19 +140,20 @@ const parent_container = document.getElementById("container");
 const footer_button = document.getElementById("footer_button");
 // console.log(parent_container);
 
+
 const createElem = () => {
     let today = new Date();
     let input_value = footer_input.value;
     let newDiv = document.createElement("div");
     let newDivContainer = document.createElement("div");
-    let newDivContainerNameTime = document.createElement("div");
+    let newDivContainerTextTime = document.createElement("div");
     let newP = document.createElement("p");
     let newImg = document.createElement("img");
     let newNameP = document.createElement("p");
     let newTimeP = document.createElement("p");
     newDiv.setAttribute("class", "container_content_user");
     newDivContainer.setAttribute("class", "container_content_p");
-    newDivContainerNameTime.setAttribute("class", "container_content_p_name_time");
+    newDivContainerTextTime.setAttribute("class", "container_content_p_text_time");
     newImg.setAttribute("src", "./assets/user-man.jpg");
     newImg.setAttribute("alt", "user_image");
     newP.setAttribute("class", "container_content_text_p");
@@ -154,10 +165,12 @@ const createElem = () => {
     newP.append(input_value);
     parent_container.appendChild(newDiv);
     newDiv.appendChild(newDivContainer);
-    newDivContainer.appendChild(newDivContainerNameTime);
-    newDivContainerNameTime.appendChild(newTimeP);
-    newDivContainerNameTime.appendChild(newNameP);
-    newDivContainer.appendChild(newP);
+
+    newDivContainer.appendChild(newNameP);
+    newDivContainer.appendChild(newDivContainerTextTime);
+    newDivContainerTextTime.appendChild(newTimeP);
+    newDivContainerTextTime.appendChild(newP);
+
     newDiv.appendChild(newImg);
     scrollControl();
 }
@@ -167,31 +180,33 @@ const matchSentenceByMin = () => {
     let today = new Date();
     let newByMinDiv = document.createElement("div");
     let newByMinDivContainer = document.createElement("div");
-    let newByMinDivContainerNameTime = document.createElement("div");
+    let newByMinDivContainerTextTime = document.createElement("div");
     let newByMinImg = document.createElement("img");
     let newByMinP = document.createElement("p");
     let newByMinNameP = document.createElement("p");
     let newByMinTimeP = document.createElement("p");
     newByMinDiv.setAttribute("class", "container_content");
     newByMinDivContainer.setAttribute("class", "container_content_p");
-    newByMinDivContainerNameTime.setAttribute("class", "container_content_p_name_time");
+    newByMinDivContainerTextTime.setAttribute("class", "container_content_p_text_time");
     newByMinImg.setAttribute("src", "./assets/minjong.png");
     newByMinImg.setAttribute("alt", "min_image");
     newByMinNameP.setAttribute("class", "container_content_name");
     newByMinTimeP.setAttribute("class", "container_content_time");
     newByMinP.setAttribute("id", "container_content_text_p");
     newByMinP.setAttribute("class", "container_content_text_p");
-    newByMinNameP.append("Min~");
+    newByMinNameP.append("Ozz min~");
     newByMinTimeP.append(`${today.getHours()}:${today.getMinutes()}`)
     newByMinP.append("꺅!!!!!!!");
     setTimeout(() => {
         parent_container.appendChild(newByMinDiv);
         newByMinDiv.appendChild(newByMinImg);
         newByMinDiv.appendChild(newByMinDivContainer);
-        newByMinDivContainer.appendChild(newByMinDivContainerNameTime);
-        newByMinDivContainerNameTime.appendChild(newByMinNameP);
-        newByMinDivContainerNameTime.appendChild(newByMinTimeP);
-        newByMinDivContainer.appendChild(newByMinP);
+
+        newByMinDivContainer.appendChild(newByMinNameP);
+        newByMinDivContainer.appendChild(newByMinDivContainerTextTime);
+        
+        newByMinDivContainerTextTime.appendChild(newByMinP);
+        newByMinDivContainerTextTime.appendChild(newByMinTimeP);
         scrollControl();
     }, 1000); 
 }
@@ -200,31 +215,34 @@ const matchSentenceByByung = () => {
     let today = new Date();
     let newByByungDiv = document.createElement("div");
     let newByByungDivContainer = document.createElement("div");
-    let newByByungDivContainerNameTime = document.createElement("div");
+    let newByByungDivContainerTextTime = document.createElement("div");
     let newByByungImg = document.createElement("img");
     let newByByungP = document.createElement("p");
     let newByByungNameP = document.createElement("p");
     let newByByungTimeP = document.createElement("p");
     newByByungDiv.setAttribute("class", "container_content");
     newByByungDivContainer.setAttribute("class", "container_content_p");
-    newByByungDivContainerNameTime.setAttribute("class", "container_content_p_name_time");
-    newByByungImg.setAttribute("src", "./assets/byung.png");
+    newByByungDivContainerTextTime.setAttribute("class", "container_content_p_text_time");
+    newByByungImg.setAttribute("src", "./assets/gong.jpg");
     newByByungImg.setAttribute("alt", "byung_image");
     newByByungNameP.setAttribute("class", "container_content_name");
     newByByungTimeP.setAttribute("class", "container_content_time");
     newByByungP.setAttribute("id", "container_content_text_p");
     newByByungP.setAttribute("class", "container_content_text_p");
-    newByByungNameP.append("Byung~");
+    newByByungNameP.append("Ozz gong~");
     newByByungTimeP.append(`${today.getHours()}:${today.getMinutes()}`)
     newByByungP.append("네이노어어ㅗㅁ오ㅗㅁㅇ노!");
     setTimeout(() => {
         parent_container.appendChild(newByByungDiv);
         newByByungDiv.appendChild(newByByungImg);
         newByByungDiv.appendChild(newByByungDivContainer);
-        newByByungDivContainer.appendChild(newByByungDivContainerNameTime);
-        newByByungDivContainerNameTime.appendChild(newByByungNameP);
-        newByByungDivContainerNameTime.appendChild(newByByungTimeP);
-        newByByungDivContainer.appendChild(newByByungP);
+
+        newByByungDivContainer.appendChild(newByByungNameP);
+        newByByungDivContainer.appendChild(newByByungDivContainerTextTime);
+
+        newByByungDivContainerTextTime.appendChild(newByByungP);
+        newByByungDivContainerTextTime.appendChild(newByByungTimeP);
+
         scrollControl();
     }, 1000);
 }
