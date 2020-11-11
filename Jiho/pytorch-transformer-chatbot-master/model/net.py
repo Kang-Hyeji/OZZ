7ec735549ccfbe193d302822663e2d7bee83a2a8
wlghsp <wlghsp@gmail.com>
2020-11-11 10:50:04 +0900

    update dataset

--- Jiho/pytorch-transformer-chatbot-master/model/net.py
+++ Jiho/pytorch-transformer-chatbot-master/model/net.py
@@ -14,11 +14,7 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 
 class Transformer(nn.Module):
     def __init__(self, config: Config, vocab: Vocabulary) -> None:
-        """Instantiating Transformer class
-        Args:
-            config (Config): model config, the instance of data_utils.utils.Config
-            vocab (Vocabulary): the instance of data_utils.vocab_tokenizer.Vocabulary
-        """
+       
         super(Transformer, self).__init__()
         self.vocab = vocab
         d_model = config.d_model #512
@@ -39,7 +35,7 @@ class Transformer(nn.Module):
 
         self.proj_vocab_layer = nn.Linear(in_features=dim_feedforward, out_features=config.vocab_size)
 
-        self.apply(self._initailze) # https://pytorch.org/docs/stable/_modules/torch/nn/modules/module.html#Module.apply
+        self.apply(self._initailze) 
 
     def forward(self, enc_input: torch.Tensor, dec_input: torch.Tensor) -> torch.Tensor:
         x_enc_embed = self.input_embedding(enc_input.long())
@@ -51,13 +47,9 @@ class Transformer(nn.Module):
         memory_key_padding_mask = src_key_padding_mask
         tgt_mask = self.transfomrer.generate_square_subsequent_mask(dec_input.size(1))
 
-        # einsum ref: https://pytorch.org/docs/stable/torch.html#torch.einsum
-        # https://obilaniu6266h16.wordpress.com/2016/02/04/einstein-summation-in-numpy/
         x_enc_embed = torch.einsum('ijk->jik', x_enc_embed)
         x_dec_embed = torch.einsum('ijk->jik', x_dec_embed)
 
-
-        # transformer ref: https://pytorch.org/docs/stable/nn.html#torch.nn.Transformer
         feature = self.transfomrer(src = x_enc_embed,
                                    tgt = x_dec_embed,
                                    src_key_padding_mask = src_key_padding_mask,
